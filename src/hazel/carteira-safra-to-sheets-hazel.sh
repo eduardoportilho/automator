4c6c66bf22cd539516ca31f463e992c9884cc9a3
Eduardo Portilho <eduardo.portilho@toptal.com>
2024-10-13 17:25:54 -0300

    carteira-safra-to-sheets-hazel

---
+++ src/hazel/carteira-safra-to-sheets-hazel.sh
@@ -0,0 +1,19 @@
+# Required environment variables:
+# - $1 (Input file path provided by Hazel)
+# - AUTOMATOR_SCRIPTS_PATH
+# - HAZEL_LOG_FILE
+
+source /Users/eduardoportilho/dev/personal/automator/.envrc
+echo "----------------------------------------------" > $HAZEL_LOG_FILE
+echo "Rule [carteira-safra-to-sheets] executed at: $(date)" >> $HAZEL_LOG_FILE
+echo "Input file: $1" >> $HAZEL_LOG_FILE
+
+# Echo command for debbuging
+echo "" >> $HAZEL_LOG_FILE
+echo "$AUTOMATOR_SCRIPTS_PATH/carteira-safra-to-sheets.ts $1" >> $HAZEL_LOG_FILE
+echo "" >> $HAZEL_LOG_FILE
+
+# Execute command
+$AUTOMATOR_SCRIPTS_PATH/carteira-safra-to-sheets.ts $1 >> $HAZEL_LOG_FILE 2>&1
+
+echo "Rule finished" >> $HAZEL_LOG_FILE
