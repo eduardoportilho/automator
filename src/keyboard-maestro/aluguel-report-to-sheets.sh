ad66083808b3e5cd980e36dde55945246d673c0d
Eduardo Portilho <eduardo.portilho@toptal.com>
2024-12-13 13:47:03 +0300

    aluguel-report-to-sheets

---
+++ src/keyboard-maestro/aluguel-report-to-sheets.sh
@@ -0,0 +1,28 @@
+# Required environment variables:
+# - $1 (Input file path provided by Hazel)
+# - AUTOMATOR_SCRIPTS_PATH
+# - HAZEL_LOG_FILE
+
+source /Users/eduardoportilho/dev/personal/automator/src/keyboard-maestro/env.sh
+source /Users/eduardoportilho/dev/personal/automator/.envrc
+echo "----------------------------------------------" > $HAZEL_LOG_FILE
+echo ""aluguel-report-to-sheets.sh" shell script action executed at: $(date)" >> $HAZEL_LOG_FILE
+
+echo "" >> $HAZEL_LOG_FILE
+
+echo "Selected file on Finder: $KMVAR_Path" >> $HAZEL_LOG_FILE
+
+echo "" >> $HAZEL_LOG_FILE
+
+# Echo command for debbuging
+# echo "" >> $HAZEL_LOG_FILE
+# echo "$AUTOMATOR_SCRIPTS_PATH/aluguel-report.ts $1" >> $HAZEL_LOG_FILE
+
+echo "" >> $HAZEL_LOG_FILE
+
+# Execute command
+$AUTOMATOR_SCRIPTS_PATH/aluguel-report-to-sheets.ts "$KMVAR_Path" >> $HAZEL_LOG_FILE 2>&1
+
+echo "" >> $HAZEL_LOG_FILE
+
+echo "Action finished" >> $HAZEL_LOG_FILE
--- src/scripts/aluguel-report-to-sheets.ts
+++ src/scripts/aluguel-report-to-sheets.ts
@@ -4,54 +4,30 @@
 //  or
 // $ chmod +x ./src/scripts/*.*
 // $ ./src/scripts/aluguel-report.ts input.pdf
-import {
-  MARIA_QUITERIA,
-  COPA_542,
-  OPEN_MALL,
-  GRANJA_BRASIL,
-  MILLENIUM,
-  GTC,
-  LEBLON,
-  CONTROLE_SPREADSHEET_ID,
-} from "../constants";
-import nodePath from "path";
-import { copyFileSync } from "fs";
+import { CONTROLE_SPREADSHEET_ID } from "../constants";
 import { getArgs } from "../utils/scripts";
-import { convertDateFormat, DMY_FORMAT } from "../utils/date/date";
 import { appendToSheet } from "../utils/sheets/sheets";
 import { readAluguelReportPdf } from "../services/read-aluguel-report-pdf/read-aluguel-report-pdf";
-
-const ALUGUEIS_ROOT_FOLDER_PATH =
-  "/Users/eduardoportilho/My Drive (eduardo.portilho@gmail.com)/_EPPCloud/__Financas/__2023/Alugueis";
-
-const ALUGUEIS_SUBFOLDER_MAP: Record<string, string> = {
-  [MARIA_QUITERIA]: "Maria Quitéria",
-  [COPA_542]: "Copa 542",
-  [OPEN_MALL]: "Open Mall",
-  [GRANJA_BRASIL]: "Granja Brasil",
-  [MILLENIUM]: "Millenium",
-  [GTC]: "Airbnb - GTC e Leblon",
-  [LEBLON]: "Airbnb - GTC e Leblon",
-};
 
 (async () => {
   try {
     const [inputPath] = getArgs({
       requiredCount: 1,
-      errorMessage: `Missing arguments. Usage: read-pdf.ts <path/to/file.pdf>`,
+      errorMessage: `Missing arguments. Usage: aluguel-report-to-sheets.ts <path/to/file.pdf>`,
     });
 
     console.log(`🔦 Reading PDF...\n`);
 
-    const { entry, isAirbnb } = await readAluguelReportPdf(inputPath);
+    const { entry } = await readAluguelReportPdf(inputPath);
 
-    console.log(`>>>---<<<`, entry, `>>>---<<<\n`);
+    // console.log(`>>>---<<<`, entry, `>>>---<<<\n`);
 
     // append to alugueis gsheet (entry):
     // - const sheetRowEntry = buildSheetRowEntry(entry)
     // - appendToSheet(sheetRowEntry, 'sheet', 'page')
 
     console.log(`🛫 Appending to "🕹️ Controle (2025) 🕹️" sheet...\n`);
+
     appendToSheet({
       spreadsheetId: CONTROLE_SPREADSHEET_ID,
       tableHeaderRangeA1: "'🏡Alugueis'!A1:J1",
@@ -70,19 +46,6 @@ const ALUGUEIS_SUBFOLDER_MAP: Record<string, string> = {
         ],
       ],
     });
-
-    const destinationPath = nodePath.join(
-      ALUGUEIS_ROOT_FOLDER_PATH,
-      ALUGUEIS_SUBFOLDER_MAP[entry.imovel],
-      convertDateFormat({
-        date: entry.dataPagamento,
-        inputFormat: DMY_FORMAT,
-        outputFormat: isAirbnb ? "yyMMdd" : "yyMM",
-      }) + ".pdf"
-    );
-
-    console.log(`📂 Copying file to "${destinationPath}"...\n`);
-    // copyFileSync(inputPath, destinationPath);
 
     console.log("✅ Done!");
   } catch (error) {
